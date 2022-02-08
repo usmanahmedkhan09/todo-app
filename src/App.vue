6350330a3cfd62ab4cf719832eab91ee64fa0445
usman ahmed <usmanahmed.andpercent@gmail.com>
2022-02-08 13:21:02 +0500

    completed

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,204 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <div class="container__item">
+      <form class="form">
+        <input
+          v-model="todo.name"
+          type="text"
+          class="form__field"
+          placeholder="Add Your Todo"
+        />
+        <button
+          @click="addTodo()"
+          type="button"
+          class="btn btn--primary btn--inside uppercase"
+        >
+          Add Todo
+        </button>
+      </form>
+    </div>
+    <ul v-for="(todo, index) in todos" :key="index">
+      <li @click="changeTodoStatus(todo)">
+        <span :class="todo.completed ? 'todo__completed' : ''">{{
+          todo.name
+        }}</span>
+        <span @click="removeTodo(todo)"> &#10007;</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import {
+  getFirestore,
+  onSnapshot,
+  setDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      todo: {
+        name: null,
+        completed: false,
+      },
+      todos: [],
+      db: getFirestore(),
+    };
+  },
+  methods: {
+    async addTodo() {
+      this.todos.push(this.todo);
+      await setDoc(doc(this.db, "todosCollection", "todos"), {
+        ...this.todos,
+      });
+      this.todo.name = null;
+    },
+    async removeTodo(payload) {
+      let updatedTodos = [...this.todos].filter(
+        (item) => item.name !== payload.name
+      );
+      await deleteDoc(doc(this.db, "todosCollection", "todos"));
+      await setDoc(doc(this.db, "todosCollection", "todos"), {
+        ...updatedTodos,
+      });
+    },
+    async changeTodoStatus(payload) {
+      let updatedTodos = [...this.todos].map((item) => {
+        if (item.name === payload.name) {
+          item.completed = true;
+        }
+        return item;
+      });
+      await setDoc(doc(this.db, "todosCollection", "todos"), {
+        ...updatedTodos,
+      });
+    },
+    getTodos() {
+      onSnapshot(doc(this.db, "todosCollection", "todos"), (doc) => {
+        const arr = doc.data();
+        if (arr) {
+          this.todos = Object.values(arr);
+        }
+      });
+    },
+  },
+  created() {
+    this.getTodos();
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+//** variables
+$background: #f5f6fa;
+$text: #9c9c9c;
+$input-bg-color: #fff;
+$input-text-color: #a3a3a3;
+$button-bg-color: #7f8ff4;
+$button-text-color: #fff;
+
+//** root
+:root {
+  background: $background;
+  color: $text;
+  font: 1rem "PT Sans", sans-serif;
+}
+
+a {
+  color: inherit;
+
+  &:hover {
+    color: $button-bg-color;
+  }
+}
+
+//** helper
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: auto;
+  width: 30%;
+  padding: 3rem;
+  margin: auto;
+  background: white;
+  border-radius: 10px;
+  -webkit-box-shadow: 0 10px 6px -6px #777;
+  -moz-box-shadow: 0 10px 6px -6px #777;
+  box-shadow: 0 13px 11px 1px #777;
+}
+
+.uppercase {
+  text-transform: uppercase;
+}
+
+//** button
+.btn {
+  display: inline-block;
+  background: transparent;
+  color: inherit;
+  font: inherit;
+  border: 0;
+  outline: 0;
+  padding: 0;
+  transition: all 200ms ease-in;
+  cursor: pointer;
+
+  &--primary {
+    background: $button-bg-color;
+    color: $button-text-color;
+    box-shadow: 0 0 10px 2px rgba(0, 0, 0, 0.1);
+    border-radius: 2px;
+    padding: 12px 36px;
+
+    &:hover {
+      background: darken($button-bg-color, 4%);
+    }
+
+    &:active {
+      background: $button-bg-color;
+      box-shadow: inset 0 0 10px 2px rgba(0, 0, 0, 0.2);
+    }
+  }
+
+  &--inside {
+    margin-left: -96px;
+  }
+}
+
+//** form
+.form {
+  &__field {
+    width: 360px;
+    background: #fff;
+    color: $input-text-color;
+    font: inherit;
+    box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.1);
+    border: 0;
+    outline: 0;
+    padding: 22px 18px;
+  }
+}
+
+ul {
+  list-style: none;
+  width: 100%;
+  cursor: pointer;
+  margin: 5px;
+  padding: 1rem;
+  background: #fff;
+  box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.1);
+
+  li {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+.todo__completed {
+  text-decoration: line-through;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
+/* eslint-disable prettier/prettier */
 import { createApp } from "vue";
 import App from "./App.vue";
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAT7gsXLYYhaXy9MwMUGUqa4A7qyjOQS-E",
+    authDomain: "todo-app-b8c9a.firebaseapp.com",
+    projectId: "todo-app-b8c9a",
+    storageBucket: "todo-app-b8c9a.appspot.com",
+    messagingSenderId: "1003095189486",
+    appId: "1:1003095189486:web:fa0c466db51158920b10ff",
+    measurementId: "G-CD8R7DTMF7",
+};
+
+initializeApp(firebaseConfig);
 
 createApp(App).mount("#app");
