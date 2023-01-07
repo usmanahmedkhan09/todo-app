aea9195c113422298ba8e9d12c9c67ee776ef26d
usmanahmedkhan09 <usmanahmedkhan@live.com>
2023-01-07 23:24:26 +0500

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <header class="header"></header>
+  <header class="header">
+    <div class="header__title">
+      <h1>TODO</h1>
+      <iconSunVue />
+    </div>
+  </header>
   <main class="content"></main>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
+import iconMoonVue from "./components/icons/icon-moon.vue";
+import iconSunVue from "./components/icons/icon-sun.vue";
 
 export default defineComponent({
+  components: { iconMoonVue, iconSunVue },
   setup() {
     return {};
   },
